2ced538f0d28b0a1ac9d79b842eff5e081e1b742
Ian Gabriel Sanchez <ian.sanchez@precisiontechpartners.com>
2018-12-12 15:48:44 +0800

    Finished a feature that calculates price of products sold by piece

--- src/test/java/com/igcs/Stepdefs.java
+++ src/test/java/com/igcs/Stepdefs.java
@@ -1,5 +1,6 @@
 package com.igcs;
 
+import com.igcs.grocery.GroceryStore;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
@@ -7,5 +8,23 @@ import cucumber.api.java.en.Then;
 import static org.junit.Assert.*;
 
 public class Stepdefs {
+    private GroceryStore store;
+    private double previousValue;
+
+    @Given("^I have started the checkout counter$")
+    public void i_have_started_the_checkout_counter() throws Exception {
+        store = new GroceryStore();
+        previousValue = store.getTotalPrice();
+    }
+
+    @When("^I add a \"([^\"]*)\"$")
+    public void i_add_a(String arg1) throws Exception {
+        store.scanProduct( arg1 );
+    }
+
+    @Then("^total price is updated to \"([^\"]*)\"$")
+    public void total_price_is_updated_to(String value) throws Exception {
+        assertEquals( Double.parseDouble( value ), store.getTotalPrice(), 0.001);
+    }
 
 }
